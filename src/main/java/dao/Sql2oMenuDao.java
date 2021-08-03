e31c948363d6581ffbe8a4a3d8f50c8082094dbd
OderoOluoch <oderoluoch@gmail.com>
2021-08-03 14:57:46 +0300

    The Api end point for the menu items now in place.

--- src/main/java/dao/Sql2oMenuDao.java
+++ src/main/java/dao/Sql2oMenuDao.java
@@ -17,7 +17,7 @@ public class Sql2oMenuDao implements MenuDao {
 
     @Override
     public void add(Menu menu) {
-        String sql = "INSERT INTO news (title,content,authorid) VALUES (:title ,:content,:authorId)"; //if you change your model, be sure to update here as well!
+        String sql = "INSERT INTO news (name) VALUES (:name)"; //if you change your model, be sure to update here as well!
         try (Connection con = sql2o.open()) {
             int id = (int) con.createQuery(sql, true)
                     .bind(menu)
@@ -34,7 +34,7 @@ public class Sql2oMenuDao implements MenuDao {
     @Override
     public List<Menu> getAll() {
         try (Connection con = sql2o.open()) {
-            return con.createQuery("SELECT * FROM news")
+            return con.createQuery("SELECT * FROM menu")
                     .executeAndFetch(Menu.class);
         }
     }
@@ -43,7 +43,7 @@ public class Sql2oMenuDao implements MenuDao {
     @Override
     public Menu findById(int id) {
         try (Connection con = sql2o.open()) {
-            return con.createQuery("SELECT * FROM news WHERE id = :id")
+            return con.createQuery("SELECT * FROM menu WHERE id = :id")
                     .addParameter("id", id)
                     .executeAndFetchFirst(Menu.class);
         }
@@ -51,25 +51,25 @@ public class Sql2oMenuDao implements MenuDao {
 
     @Override
     public void deleteById(int id) {
-        String sql = "DELETE from news WHERE id = :id";
-        String deleteJoin = "DELETE from news_depatments WHERE newsid = :newsid";
-        try (Connection con = sql2o.open()) {
-            con.createQuery(sql)
-                    .addParameter("id", id)
-                    .executeUpdate();
-            con.createQuery(deleteJoin)
-                    .addParameter("newsid", id)
-                    .executeUpdate();
-
-        } catch (Sql2oException ex){
-            System.out.println(ex);
-        }
+//        String sql = "DELETE from menu WHERE id = :id";
+//        String deleteJoin = "DELETE from news_depatments WHERE newsid = :newsid";
+//        try (Connection con = sql2o.open()) {
+//            con.createQuery(sql)
+//                    .addParameter("id", id)
+//                    .executeUpdate();
+//            con.createQuery(deleteJoin)
+//                    .addParameter("newsid", id)
+//                    .executeUpdate();
+//
+//        } catch (Sql2oException ex){
+//            System.out.println(ex);
+//        }
 
     }
 
     @Override
     public void clearAll() {
-        String sql = "DELETE from news";
+        String sql = "DELETE from menu";
         try (Connection con = sql2o.open()) {
             con.createQuery(sql).executeUpdate();
         } catch (Sql2oException ex) {
