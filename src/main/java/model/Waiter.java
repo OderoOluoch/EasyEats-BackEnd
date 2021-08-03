efcc387369af09ea62a743e3254872ec046e05f1
OderoOluoch <oderoluoch@gmail.com>
2021-08-03 15:48:55 +0300

    Model class for the waiter object in place

---
+++ src/main/java/model/Waiter.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.util.Objects;
+
+public class Waiter {
+    private int id;
+    private String name;
+
+    public Waiter(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Waiter waiter = (Waiter) o;
+        return id == waiter.id && Objects.equals(name, waiter.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
