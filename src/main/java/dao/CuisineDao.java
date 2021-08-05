efbc0c1407af84f4e34c5cd319343ad6b5599783
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-08-05 12:54:30 +0300

    Add  Create the Dao interface for Cuisine class

---
+++ src/main/java/dao/CuisineDao.java
@@ -0,0 +1,23 @@
+package dao;
+
+import model.Cuisine;
+
+import java.util.List;
+
+public interface CuisineDao {
+    void save();
+
+    //CREATE
+    void add(Cuisine cuisine);
+
+    //READ
+    List<Cuisine> getAll();
+    Cuisine findById(int id);
+
+    //UPDATE
+    void update(Cuisine cuisine,int id, String FoodType,int price, int MenuId);
+
+    //DELETE
+    void deleteById();
+    void clearAllTasks();
+}
